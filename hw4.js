bce442786395d88127c6531d188c1d99176adf8c
Jane <cjmendez26@gmail.com>
2019-11-16 21:30:15 -0800

    new

--- hw4.js
+++ hw4.js
@@ -1,75 +1,161 @@
+//js file
 
 
-
-const start = DocumentType.getElementById("start");
-
-const quiz = DocumentType.getElementById("quiz");
-
-const qimg = DocumentType.getElementById("questionImage");
-
-const question = DocumentType.getElementById("question");
-
-const counter = DocumentType.getElementById("counter");
-
-const timeGuage = DocumentType.getElementById("timeGuage");
-
-const choiceA = DocumentType.getElementById("A");
-
-const choiceB = DocumentType.getElementById("B");
-
-const choiceC = DocumentType.getElementById("C");
-
-const progress = DocumentType.getElementById("progress");
-
-const scoreDiv= DocumentType.getElementById("score");
-
+//set variables by element id
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
+// Qusetions with choice answers
 let questions=[
   
-  { question:"",
-    image:"",
-    choiceA:"",
-    choiceB:"",
-    choiceC:"",
-    correct:""
+  { question:"Which famous scientist introduced the idea of natural selection?",
+    choiceA:"Sigmund Freud",
+    choiceB:"Charles Darwin",
+    choiceC:"Louis Pasteur",
+    correct:"B"
   },
-  { question:"",
-    image:"",
-    choiceA:"",
-    choiceB:"",
-    choiceC:"",
-    correct:""
+  { question:"A person who studies biology is known as a?",
+    choiceA:"Botanist",
+    choiceB:"Mycologist",
+    choiceC:"Biologist",
+    correct:"C"
   },
-  { question:"",
-    image:"",
-    choiceA:"",
-    choiceB:"",
-    choiceC:"",
-    correct:""
+  { question:" Botany is the study of?",
+    choiceA:"Animals",
+    choiceB:"Cells",
+    choiceC:"Plants",
+    correct:"C"
   },
-  { question:"",
-    image:"",
-    choiceA:"",
-    choiceB:"",
-    choiceC:"",
-    correct:""
+  { question:"Can frogs live in salt water?",
+    choiceA:"Yes",
+    choiceB:"No",
+    choiceC:"Both",
+    correct:"B"
   },
-  { question:"",
-    image:"",
-    choiceA:"",
-    choiceB:"",
-    choiceC:"",
-    correct:""
+  { question:"Animals which eat both plants and other animals are known as what?",
+    choiceA:"Omnivores",
+    choiceB:"Carnivore",
+    choiceC:"Herbivores",
+    correct:"A"
   }
 ];
-
-const lastQuestion = question.lenght -1;
+const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
+let count = 0;
+const questionTime = 10; // 10s
+const gaugeWidth = 150; // 150px
+const gaugeUnit = gaugeWidth / questionTime;
+let TIMER;
+let score = 0;
 
+// render a question
 function renderQuestion(){
-  let q = questions[runningQuestion];
-  question.innerHTML = "<p>"+ q.question +"</p>";
-  qimg.innerHTML = "<img scr="+ q.imgSrc +">";
-  choiceA.innerHTML = q.choiceA;
-  choiceB.innerHTML = q.choiceB;
-  choiceC.innerHTML = q.choiceC;
+    let q = questions[runningQuestion];
+    
+    question.innerHTML = "<p>"+ q.question +"</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
 }
+
+start.addEventListener("click",startQuiz);
+
+// start quiz
+function startQuiz(){
+    start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderProgress();
+    renderCounter();
+    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+}
+
+// render progress
+function renderProgress(){
+    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    }
+}
+
+// counter render
+
+function renderCounter(){
+    if(count <= questionTime){
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count++
+    }else{
+        count = 0;
+        // change progress color to red
+        answerIsWrong();
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
+}
+
+// checkAnwer
+
+function checkAnswer(answer){
+    if( answer == questions[runningQuestion].correct){
+        // answer is correct
+        score++;
+        // change progress color to green
+        answerIsCorrect();
+    }else{
+        // answer is wrong
+        // change progress color to red
+        answerIsWrong();
+    }
+    count = 0;
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion();
+    }else{
+        // end the quiz and show the score
+        clearInterval(TIMER);
+        scoreRender();
+    }
+}
+
+// answer is correct
+function answerIsCorrect(){
+    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+}
+
+// answer is Wrong
+function answerIsWrong(){
+    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+}
+
+// score render
+function scoreRender(){
+    scoreDiv.style.display = "block";
+    
+    // calculate the amount of question percent answered by the user
+    const scorePerCent = Math.round(100 * score/questions.length);
+    
+    // choose the image based on the scorePerCent
+    
+    
+    scoreDiv.innerHTML = "score";
+    scoreDiv.innerHTML += "score";
+}
+
+
+}
+
+
+
